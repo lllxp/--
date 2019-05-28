a4fba69626327b4e0557a149a8a91ff97224523d
lllxp <1261543653@qq.com>
2019-05-28 23:22:57 +0800

    容器与泛型第一题 first commit

---
+++ MyProject_01/src/容器与泛型/Test_1.java
@@ -0,0 +1,14 @@
+package ÈÝÆ÷Óë·ºÐÍ;
+class Student{
+3		int stuNo;
+4		String name;
+5		int score;
+6	}
+7	public class test_1 {
+8	
+9		public static void main(String[] args) {
+10			// TODO Auto-generated method stub
+11	
+12		}
+13	
+14	}
