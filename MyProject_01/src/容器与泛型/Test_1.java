ced4efa7d350c2d8fe372a0f49c387dcca33182b
lllxp <1261543653@qq.com>
2019-05-28 23:53:44 +0800

    容器与泛型第一题 third commit

--- MyProject_01/src/容器与泛型/Test_1.java
+++ MyProject_01/src/容器与泛型/Test_1.java
@@ -1,9 +1,9 @@
 package 容器与泛型;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.Iterator;
 
-
-class Student{
+class Student extends Object implements Comparable<Student>{
 	int stuNo;
 	String name;
 	int score;
@@ -14,15 +14,46 @@ class Student{
 		this.name = name;
 		this.score = score;
 	}
+
+@Override
+public int compareTo(Student s) {
+	// TODO Auto-generated method stub
+	if (this.stuNo == s.stuNo)
+		return 0;
+	else if (this.score == s.score) {
+		if (this.name.compareTo(s.name) == 0)
+			return this.stuNo - s.stuNo;
+		else
+			return this.name.compareTo(s.name);
+	} else
+		return this.score - s.score;
 }
+
+@Override
+public String toString() {
+	return "Student [stuNo=" + stuNo + ", name=" + name + ", score=" + score + "]";
+}
+
+}
+
 public class Test_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Set<Student> set=new TreeSet<Student>();
-		
+		Student s1 = new Student(01, "张三", 95);
+		set.add(s1);
+		Student s2 = new Student(02, "李四", 86);
+		set.add(s2);
+		Student s3 = new Student(03, "王五", 77);
+		set.add(s3);
+
+		Iterator<Student> it= set.iterator();
+		while( it.hasNext() ) {
+			Student s4=it.next();
+		}
+		System.out.println(set);
 		
 	}
-
 }
