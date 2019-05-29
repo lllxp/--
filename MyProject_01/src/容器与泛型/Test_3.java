2169aaf5c1973107ba2c40890bc0e80f662d07bd
lllxp <1261543653@qq.com>
2019-05-29 16:41:30 +0800

    first commit

---
+++ MyProject_01/src/容器与泛型/Test_3.java
@@ -0,0 +1,22 @@
+package ÈİÆ÷Óë·ºĞÍ;
+
+class Candidate{
+	
+	String name;
+	int count;
+	public Candidate(String name, int count) {
+		super();
+		this.name = name;
+		this.count = count;
+	}
+	
+}
+
+public class Test_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
