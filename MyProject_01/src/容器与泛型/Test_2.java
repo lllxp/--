a6b6563697cab32efb2ede559568baa0eb076d4e
lllxp <1261543653@qq.com>
2019-05-29 16:14:41 +0800

    first commit

---
+++ MyProject_01/src/容器与泛型/Test_2.java
@@ -0,0 +1,28 @@
+package ÈÝÆ÷Óë·ºÐÍ;
+
+public class Test_2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
+class Student_1 {
+	String name;
+	int age;
+	public Student_1(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
