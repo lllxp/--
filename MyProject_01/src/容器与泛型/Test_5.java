07c418532eaca4baa6ecb5231398c4b0baa9d9af
lllxp <1261543653@qq.com>
2019-05-29 21:39:08 +0800

    third commit

--- MyProject_01/src/容器与泛型/Test_5.java
+++ MyProject_01/src/容器与泛型/Test_5.java
@@ -1,13 +1,13 @@
 package ÈİÆ÷Óë·ºĞÍ;
 
-class Student2{	
-	int id;	
+class Student2 implements Comparable<Object>{
+	String id;	
 	String name;	
 	int count;	
-	public int getId() {	
+	public String getId() {	
 		return id;	
 	}
-	public void setId(int id) {	
+	public void setId(String id) {	
 		this.id = id;		
 	}
 	public String getName() {	
@@ -26,19 +26,46 @@ class Student2{
 	public String toString() {
 		return "Student2 [id=" + id + ", name=" + name + ", count=" + count + "]";
 		}
-	public Student2(int id, String name, int count) {
+	public Student2(String id, String name, int count) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.count = count;
 		}
+	@Override
+	public int hashCode() {
+		int result;
+		result = (this.name == null ? 0 : this.name.hashCode());
+		result += this.id.hashCode();
+		return result;
+	}
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof Test_5)) {
+			return false;
+			}
+		Test_5 s = (Test_5) o;
+		if (this.id.equals(s.id)) {
+			return true;
+			} else {
+				return false;
+				}
+		}
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		Student2 s = (Student2) o;
+		int result = 0;
+		result = this.count > s.count ? 1 : (this.count == s.count ? 0 : -1);
+		return result;
+		}
 	}
 
-public class Test_5 {
+public class Test_5 {	
 	
+	public Object id;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
 }
