22a893a3ebda01412b59928238cef9a3800849d1
lllxp <1261543653@qq.com>
2019-05-29 16:44:39 +0800

    second commit

--- MyProject_01/src/容器与泛型/Test_3.java
+++ MyProject_01/src/容器与泛型/Test_3.java
@@ -1,4 +1,5 @@
 package 容器与泛型;
+import java.util.HashMap;
 
 class Candidate{
 	
@@ -16,7 +17,10 @@ public class Test_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		HashMap map = new HashMap();
+		Candidate p1 = new Candidate("张三", 0);
+		Candidate p2 = new Candidate("李四", 0);
+		Candidate p3 = new Candidate("王五", 0);
 	}
 
 }
