4b6486bb5dd9ff35cb31e5b5dd7b738c89a0142d
lllxp <1261543653@qq.com>
2019-05-29 16:28:13 +0800

    third commit

--- MyProject_01/src/容器与泛型/Test_2.java
+++ MyProject_01/src/容器与泛型/Test_2.java
@@ -1,8 +1,21 @@
 package 容器与泛型;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Map<Student_1, String> map = new HashMap<Student_1, String>();
+		
+		Student_1 s1 = new Student_1("张三",19);
+		Student_1 s2 = new Student_1("李四",20);
+		Student_1 s3 = new Student_1("王五",21);
+		
+		map.put(s1,"归属地：福州");
+		map.put(s2,"归属地：宁德");
+		map.put(s3,"归属地：厦门");
+		
+		System.out.println(map);
 	}
 }
 class Student_1 {
