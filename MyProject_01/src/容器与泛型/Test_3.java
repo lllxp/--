396a7c6d0615354c51315a7450788f489ab1e6b8
lllxp <1261543653@qq.com>
2019-05-29 16:53:37 +0800

    third commit

--- MyProject_01/src/容器与泛型/Test_3.java
+++ MyProject_01/src/容器与泛型/Test_3.java
@@ -1,5 +1,6 @@
 package 容器与泛型;
 import java.util.HashMap;
+import java.util.Scanner;
 
 class Candidate{
 	
@@ -18,9 +19,32 @@ public class Test_3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		HashMap map = new HashMap();
-		Candidate p1 = new Candidate("张三", 0);
-		Candidate p2 = new Candidate("李四", 0);
-		Candidate p3 = new Candidate("王五", 0);
+		Candidate p1 = new Candidate("张三", 1);
+		Candidate p2 = new Candidate("李四", 2);
+		Candidate p3 = new Candidate("王五", 3);
+		
+		map.put(1, 0);
+		map.put(2, 0);
+		map.put(3, 0);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("输入10个选举人提交的候选人编号（用空格隔开）：");
+		String temp = sc.nextLine();
+		String[] t = temp.split(" ");
+		int[] num = new int[20];	
+		for (int i = 0; i < t.length; i++) {
+			
+			num[i] = Integer.parseInt(t[i]);
+			
+		}
+		for(int i=0;i<num.length;i++) {
+
+			Integer j = (Integer) map.get(num[i]);
+
+			if(map.containsKey(num[i]))
+
+				map.put(num[i],++j);
+		}
+		map.forEach((key,value)->System.out.println(key+ "号候选的得票数为："+value+"票"));
 	}
 
 }
