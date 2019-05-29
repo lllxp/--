3da85f27b54ff302bd9c9d8375d9ee0d7b5bfb92
lllxp <1261543653@qq.com>
2019-05-29 20:46:24 +0800

    first commit

---
+++ MyProject_01/src/容器与泛型/Test_5.java
@@ -0,0 +1,34 @@
+package ÈÝÆ÷Óë·ºÐÍ;
+
+class Student2{	
+	int id;	
+	String name;	
+	int count;	
+	public int getId() {	
+		return id;	
+	}
+	public void setId(int id) {	
+		this.id = id;		
+	}
+	public String getName() {	
+		return name;	
+	}
+	public void setName(String name) {	
+		this.name = name;	
+	}
+	public int getCount() {	
+		return count;	
+	}
+	public void setCount(int count) {	
+		this.count = count;	
+	}	
+}
+
+public class Test_5 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
